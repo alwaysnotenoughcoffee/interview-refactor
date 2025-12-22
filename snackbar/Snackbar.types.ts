83abbd6f9b7d17f355397e91c6ee6fff44a3ea71
TylerG468 <tyler.goulski@ext.nexthink.com>
2025-12-22 11:34:32 -0700

    Add files via upload

---
+++ snackbar/Snackbar.types.ts
@@ -0,0 +1,25 @@
+export interface SnackBarVariantProps {
+  variant?: 'default' | 'dark' | 'light';
+}
+
+export interface SnackBarProps extends SnackBarVariantProps {
+  id?: string;
+  message?: string;
+  announcements?: Announcement[];
+  closable?: boolean;
+  classNames: SnackBarClasses;
+}
+
+export interface Announcement {
+  overline?: string;
+  message: string;
+}
+
+export interface SnackBarClasses {
+  root: string;
+  container: string;
+  content: string;
+  overline: string;
+  message: string;
+  close: string;
+}
